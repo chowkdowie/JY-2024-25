80f607b87f9d20c5f264d37b1377250f8b6722a9
j47yi <j47yi@uwaterloo.ca>
2025-01-17 01:49:17 -0500

    modified:   art.html 	modified:   auav.html 	modified:   bdc.html 	modified:   css/style.css 	modified:   electrium.html 	modified:   graphic.html 	modified:   ins.html 	modified:   js/script.js 	new file:   public/fonts/moliga-demo.regular.ttf 	new file:   public/fonts/moliga-demo.regular.woff2 	modified:   vehicle.html

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ function copyEmail(){
 
 }
 
+
+
 function typeText(elementID, text, speed){
     const element = document.getElementById(elementID);
     let index = 0;
@@ -20,23 +22,32 @@ function typeText(elementID, text, speed){
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    function updateButtonText() {
+        const button = document.querySelector('.link.green');
+        if (window.innerWidth <= 1493) {
+            button.textContent = 'EMAIL';
+        } else {
+            button.textContent = 'COPY MY EMAIL';
+        }
+    }
+
+    updateButtonText();
+    window.addEventListener('resize', updateButtonText);
+
     setTimeout(() => {
         typeText("typewriter-1", "HELLO", 150);
-    }, 800)
-    
+    }, 800);
+
     setTimeout(() => {
         typeText("typewriter-2", "I’m Jennifer, currently studying Systems Design Engineering @UWaterloo and passionate about design + innovation. Let’s connect :)  ", 40);
-    }, 2000)
-});
+    }, 2000);
 
-document.addEventListener('DOMContentLoaded', () => {
     VanillaTilt.init(document.querySelectorAll('.big-works, .small-works'), {
         max: 6,
-        speed: 400, 
+        speed: 400,
         glare: true,
         "max-glare": 0.5,
     });
-
 
     const folder = document.getElementById('folder');
     const folderTextContainer = document.querySelector('.folder-text');
@@ -52,7 +63,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
-    
 
     folderTextContainer.innerHTML = `
         <span class="link yellow">UX DESIGNER</span>
@@ -63,8 +73,6 @@ document.addEventListener('DOMContentLoaded', () => {
         <span class="link white">JAN - APR 2024</span>
     `;
 
-    // folderTextContainer.classList.add('animate');
-
     folderTextContainer.addEventListener('animationed', () => {
         folderTextContainer.classList.remove('animate');
     });
@@ -72,11 +80,11 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', userScroll);
 });
 
-function switchTab(tabName){
+function switchTab(tabName) {
     const folder = document.getElementById('folder');
     const folderTextContainer = folder.querySelector('.folder-text');
 
-    if (tabName === 'community'){
+    if (tabName === 'community') {
         folder.style.backgroundColor = '#4B67E5';
         folderTextContainer.innerHTML = `
             <span class="link yellow">GRAPHIC DESIGNER</span>
@@ -84,12 +92,10 @@ function switchTab(tabName){
             <span class="link yellow">OCT 2024 - PRESENT</span>
             <span class="link white">WEB DEVELOPER</span>
             <span class="link white">ELECTRIUM MOBILITY</span>
-            <span class="link white">MAY - JUL 2024</span> 
-            `;
-
-    }
-    else{
-        folder.style.backgroundColor= 'black';
+            <span class="link white">MAY - JUL 2024</span>
+        `;
+    } else {
+        folder.style.backgroundColor = 'black';
         folderTextContainer.innerHTML = `
             <span class="link yellow">UX DESIGNER</span>
             <span class="link yellow">TREASURY BOARD SECRETARIAT</span>
@@ -97,8 +103,6 @@ function switchTab(tabName){
             <span class="link white">UAV DESIGN ENGINEER</span>
             <span class="link white">AUAV TECH</span>
             <span class="link white">JAN - APR 2024</span>
-            `;
+        `;
     }
-
-   
 }
